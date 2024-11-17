91c859e4a7df7b4c7f83d317f509c2ee7657d6ed
Rogelio Martinez <rogejr89@gmail.com>
2024-11-16 21:55:01 -0500

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+
+function Login() {
+    return (
+        <section>
+            <h1>Login</h1>
+        </section>
+    ); 
+} 
+export default Login;
+
+
+
+
+
+
